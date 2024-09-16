429997aa9fe66edc5b2cae6c632a359791cf5e40
HaSha Laptop <whitebluej@kaist.ac.kr>
2024-09-17 03:07:12 +0900

    [ChatBot] Basic Structure of Chat Object

---
+++ systemics/chatbot/chatbotError.py
@@ -0,0 +1,9 @@
+class ChatbotError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ChatbotInputError(ChatbotError):
+    def __init__(self, message):
+        super().__init__(message)
