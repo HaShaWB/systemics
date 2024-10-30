ed90870b111d76a005147d58d6c85b594033240f
HaShaWB <whitebluej@kaist.ac.kr>
2024-10-30 21:57:49 +0900

    minor

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="systemics",
-    version="0.1.11",
+    version="0.2.1",
     packages=find_packages(),
     install_requires=["numpy"],
     extras_require={
